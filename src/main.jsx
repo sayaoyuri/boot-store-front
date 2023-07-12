b1100dca1b3fc24abf48ec17ee9fc5e0042d5fce
sayaoyuri <yuriantunes0000@gmail.com>
2023-07-12 19:18:54 -0300

    Chore: Clean project structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import ResetStyle from './assets/styles/ResetStyle.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ResetStyle></ResetStyle>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
